95e3396897d1b31d4ec11c94fd8e568ef2585178
Millisa G <135849424+millisag@users.noreply.github.com>
2024-04-04 20:15:49 -0400

    Set up front end with CRUD function in job component

---
+++ src/app/job_component/job.model.ts
@@ -0,0 +1,8 @@
+export class Job {
+  id: string;
+  url: string;
+  title: string;
+  by: string;
+  time: number;
+  company: string;
+}
---
+++ src/app/job_component/job_component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Job } from './job.model';
+import { JobService } from '../job.service';
+
+@Component({
+  selector: 'app-job',
+  templateUrl: './job.component.html',
+  styleUrls: ['./job.component.css']
+})
+export class JobComponent implements OnInit {
+  jobs: Job[];
+  job: Job = {
+    title: '',
+    company: '',
+    id: '',
+    url: '',
+    by: '',
+    time: 0
+  }; // Initialize empty job
+  showForm = false; // Flag to show/hide form
+  isNewJob = true; // Flag to determine if creating or editing job
+
+  constructor(private jobService: JobService) { }
+
+  ngOnInit(): void {
+    this.getJobs();
+  }
+
+  getJobs(): void {
+    this.jobService.getJobs().subscribe(jobs => {
+      this.jobs = jobs;
+    });
+  }
+
+  editJob(id: number): void {
+    this.jobService.getJobById(id).subscribe(job => {
+      this.job = job;
+      this.isNewJob = false;
+      this.showForm = true;
+    });
+  }
+
+  createJob(): void {
+    this.jobService.createJob(this.job).subscribe(() => {
+      this.resetForm();
+      this.getJobs();
+    });
+  }
+
+  updateJob(): void {
+    this.jobService.updateJob(this.job.id, this.job).subscribe(() => {
+      this.resetForm();
+      this.getJobs();
+    });
+  }
+
+  resetForm(): void {
+    this.job = { title: '', company: '', id: '', url: '', by: '', time: 0 }; // Reset job object
+    this.isNewJob = true;
+    this.showForm = false;
+  }
+}
